6a2e6d8e063547497169bc28dbe252c2cf04d5fc
Sanskar Sahu <sanskarsahu451@gmail.com>
2025-12-12 01:16:01 +0530

    made some UI updates

--- login.js
+++ login.js
@@ -1,1 +1,25 @@
-//login
+import { auth } from './firebaseconfig.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+//----- Login code start	  
+	  document.getElementById("login-form").addEventListener("submit", function() {
+		  var email =  document.getElementById("username").value;
+		  var password = document.getElementById("password").value;
+
+		  signInWithEmailAndPassword(auth, email, password)
+		  .then((userCredential) => {
+		    // Signed in 
+		    const user = userCredential.user;
+		    console.log(user);
+		    alert(user.email+" Login successfully!!!");
+		    document.getElementById('logout').style.display = 'block';
+		    // ...
+		  })
+		  .catch((error) => {
+		    const errorCode = error.code;
+		    const errorMessage = error.message;
+		    console.log(errorMessage);
+		    alert(errorMessage);
+		  });		  		  
+	  });
+	  //----- End of Login code
